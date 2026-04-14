59c9e3548b2eee9d8b4127a0c348e2adfafd0227
akgom <132290469+akgom@users.noreply.github.com>
2026-04-14 14:17:11 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+radio.setGroup(7)
+
+roversaPet.setUltrasonicPins(DigitalPin.P13, DigitalPin.P14)
+roversaPet.setStopDistance(15)
+roversaPet.setCautionDistance(25)
+roversaPet.setPetStats(70, 20, 80)
+
+radio.onReceivedString(function (receivedString: string) {
+    if (receivedString == "pet") {
+        roversaPet.petRobot()
+        roversaPet.updateFace()
+        Roversa.stop()
+        basic.pause(500)
+    } else if (receivedString == "wave") {
+        roversaPet.calmRobot()
+        roversaPet.updateFace()
+    }
+})
+
 basic.forever(function () {
-	
+    roversaPet.updatePetState()
+    roversaPet.updateFace()
+
+    if (roversaPet.dangerDetected()) {
+        Roversa.stop()
+    } else {
+        Roversa.driveForward()
+    }
+
+    basic.pause(500)
 })
